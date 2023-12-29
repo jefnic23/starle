d9c8099f5472c6647772c041028d487ab4666c0f
Jeffrey Nicholas <jefnic23@gmail.com>
2023-12-28 20:50:24 -0500

    🐛 fix(game.py): remove actors with less than 3 movies from the list to prevent errors ✨ feat(game.py): update actor viability status and remove non-viable actors from the list to ensure only viable actors are considered for starle creation 🔧 chore(game.py): refactor movie selection logic to use vote count instead of popularity for more accurate results 🔧 chore(game.py): refactor _get_weights function to use Any type for NDArray to improve flexibility and compatibility

--- backend/services/game.py
+++ backend/services/game.py
@@ -1,8 +1,10 @@
+from typing import Any
+
 import numpy as np
-from numpy.typing import NDArray, float
+from numpy.typing import NDArray
 
 from backend.schemas.starle import Movie, Starle
-from backend.services.db import get_actors
+from backend.services.db import get_actors, update_actor
 from backend.services.tmdb import get_filtered_movies
 
 
@@ -12,49 +14,55 @@ async def create_game(seed: int) -> list[Starle]:
 
     starles = []
 
-    # TODO: if an actor doesn't have 3 movies, remove them from the list
+    rng = np.random.default_rng(seed=seed)
 
-    np.random.default_rng(seed=seed)
-    filtered_actors = np.random.choice(
-        actors,
-        size=6,
-        replace=False,
-        p=_get_weights([actor.popularity for actor in actors]),
-    )
-
-    for actor in filtered_actors:
-        filtered_movies = await get_filtered_movies(actor.id)
-
-        np.random.default_rng(seed=seed)
-        movies = sorted(
-            np.random.choice(
-                filtered_movies,
-                size=3,
-                replace=False,
-                p=_get_weights([movie.popularity for movie in filtered_movies]),
-            ),
-            key=lambda movie: movie.popularity,
-            reverse=True,
+    while len(starles) < 6:
+        filtered_actors = rng.choice(
+            actors,
+            size=6,
+            replace=False,
+            p=_get_weights([actor.popularity for actor in actors]),
         )
 
-        starle = Starle(
-            name=actor.name,
-            gender=actor.gender,
-            movies=[
-                Movie(
-                    title=movie.title,
-                    release_year=movie.release_date.year,
-                    poster_path=f"https://image.tmdb.org/t/p/original{movie.poster_path}",
-                )
-                for movie in movies
-            ],
-        )
+        for actor in filtered_actors:
+            filtered_movies = await get_filtered_movies(actor.id)
 
-        starles.append(starle)
+            if len(filtered_movies) < 3:
+                actor.viable = 0
+                update_actor(actor)
+                actors = [actor for actor in actors if actor.viable == 1]
+                continue
+
+            movies = sorted(
+                rng.choice(
+                    filtered_movies,
+                    size=3,
+                    replace=False,
+                    p=_get_weights([movie.vote_count for movie in filtered_movies]),
+                ),
+                key=lambda movie: movie.vote_count,
+                reverse=True,
+            )
+
+            starle = Starle(
+                name=actor.name,
+                gender=actor.gender,
+                movies=[
+                    Movie(
+                        title=movie.title,
+                        release_year=movie.release_date.year,
+                        poster_path=f"https://image.tmdb.org/t/p/original{movie.poster_path}",
+                    )
+                    for movie in movies
+                ],
+            )
+
+            starles.append(starle)
 
     return starles
 
 
-def _get_weights(population: list[float]) -> NDArray[float]:
+def _get_weights(population: list[float]) -> NDArray[Any]:
+    """Calculate and return normalized weights from a list of population values."""
     probability = np.array(population)
     return probability / probability.sum()
